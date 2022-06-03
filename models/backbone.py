f4bf590d33ea64af751a630fb4982afbf21f688f
lgemc <81768438+lgemc@users.noreply.github.com>
2022-06-03 16:13:40 -0500

    Force backbone to load without weights

--- models/backbone.py
+++ models/backbone.py
@@ -88,7 +88,7 @@ class Backbone(BackboneBase):
                  dilation: bool):
         backbone = getattr(torchvision.models, name)(
             replace_stride_with_dilation=[False, False, dilation],
-            pretrained=is_main_process(), norm_layer=FrozenBatchNorm2d)
+            pretrained=False, norm_layer=FrozenBatchNorm2d) # TODO: find a better way to create backbone with pretrained as a usable param
         num_channels = 512 if name in ('resnet18', 'resnet34') else 2048
         super().__init__(backbone, train_backbone, num_channels, return_interm_layers)
 
